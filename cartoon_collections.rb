934db7d527a4fdf49f0d7705a7b1eb8f299fcec7
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-23 21:28:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
    if foods.include?(cheese_types)
-     foods.each
+     foods.each_with_index {|food,i| 
   end
 end
